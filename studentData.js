24d32d291ef333bbbf2a33f15484b02268bc7500
Edward Diaz <diazedward24@gmail.com>
2023-11-24 23:09:59 -0800

    Add studentData with array and objects. Export array.

---
+++ studentData.js
@@ -0,0 +1,28 @@
+const studentData = [
+    { 
+        id: 1,
+        name: "John Lennon",
+        address: "251 Menlove Avenue",
+        averageGrade: 75,
+    },
+    { 
+        id: 2,
+        name: "Paul McCartney",
+        address: "20 Forthlin Road",
+        averageGrade: 90,
+    },
+    { 
+        id: 3,
+        name: "George Harrison",
+        address: "12 Arnold Grove",
+        averageGrade: 88,
+    },
+    { 
+        id: 4,
+        name: "Richard Starkey",
+        address: "10 Admiral Grove",
+        averageGrade: 80,
+    },
+]
+
+export default studentData;
