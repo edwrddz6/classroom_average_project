d70f4e950a58ffab030d7021ec7e767e8aa64792
Edward Diaz <diazedward24@gmail.com>
2023-11-25 12:34:29 -0800

    Remove import; unnecessary because studentData is being passed as an argument

--- calculateClassroomAverage.js
+++ calculateClassroomAverage.js
@@ -1,8 +1,6 @@
-function calculateClassroomAverage(studentData) {
-    const totalGrades = students.reduce((sum, student) => sum + student.averageGrade, 0);
-    const averageGrade = totalGrades / students.length;
+export function calculateClassroomAverage(studentData) {
+    const totalGrades = studentData.reduce((sum, student) => sum + student.averageGrade, 0);
+    const averageGrade = totalGrades / studentData.length;
 
     return averageGrade;
 }
-
-export { calculateClassroomAverage };
