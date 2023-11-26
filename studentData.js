316834a198be065646a09e7eeaef6c9c136d1d87
Edward Diaz <diazedward24@gmail.com>
2023-11-25 20:19:12 -0800

    Add more students

--- studentData.js
+++ studentData.js
@@ -22,6 +22,42 @@ const studentData = [
         name: "Richard Starkey",
         address: "10 Admiral Grove",
         averageGrade: 80,
+    },
+    { 
+        id: 5,
+        name: "Robert Plant",
+        address: "64 Causey Farm Road",
+        averageGrade: 85,
+    },
+    { 
+        id: 6,
+        name: "Jimmy Page",
+        address: "34 Miles Rd",
+        averageGrade: 90,
+    },
+    { 
+        id: 7,
+        name: "John Paul Jones",
+        address: "1243 Main Street",
+        averageGrade: 95,
+    },
+    { 
+        id: 8,
+        name: "John Bonham",
+        address: "84 Birchfield Road",
+        averageGrade: 68,
+    },
+    { 
+        id: 9,
+        name: "David Bowie",
+        address: "40 Stansfield Road",
+        averageGrade: 91,
+    },
+    { 
+        id: 10,
+        name: "Elton John",
+        address: "55 Pinner Hill Road",
+        averageGrade: 90,
     }
 ];
 
